e8cba58482a6f1b376a5ce9c36618fc9ac1edcf5
刘堂臣 <liutangchen@liutangchendeMacBook-Pro.local>
2020-10-26 17:22:04 +0800

    494

---
+++ src/com/wangfu/Leet494.java
@@ -0,0 +1,22 @@
+package com.wangfu;
+
+public class Leet494 {
+    int count = 0;
+
+    public int findTargetSumWays(int[] nums, int S) {
+        calculate(nums, 0, 0, S);
+        return count;
+    }
+
+    private void calculate(int[] nums, int i, int sum, int s) {
+        if (i == nums.length) {
+            if (sum == s) {
+                count++;
+            }
+        } else {
+            calculate(nums, i + 1, sum + nums[i], s);
+            calculate(nums, i + 1, sum - nums[i], s);
+        }
+    }
+
+}

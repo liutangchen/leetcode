e7113b495327439ab182be9e3142caf056fdaac1
liutangchen <1253584040@qq.com>
2021-05-07 12:00:00 +0800

    718

--- src/com/wangfu/Leet110.java
+++ src/com/wangfu/Leet110.java
@@ -14,4 +14,25 @@ public class Leet110 {
             return 0;
         return Math.max(high(root.left), high(root.right)) + 1;
     }
+
+    /**
+     * 方法二：一次计算树的高度就出结果
+     */
+    boolean result = true;
+
+    public boolean isBalanced2(TreeNode root) {
+        high(root);
+        return result;
+    }
+
+    public int high2(TreeNode root) {
+        if (root == null)
+            return 0;
+        int lhigh = high2(root.left);
+        int rhigh = high2(root.right);
+        if (Math.abs(lhigh - rhigh) > 1) {
+            result = false;
+        }
+        return Math.max(lhigh, rhigh) + 1;
+    }
 }
